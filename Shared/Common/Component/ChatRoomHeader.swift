7031853e001a6231cfdda2cdbfd35620891a2a84
Agus Adi <gusadi872@gmail.com>
2022-09-08 11:23:17 +0700

    [iOS] initial add chat room header view for component

---
+++ Shared/Common/Component/ChatRoomHeader.swift
@@ -0,0 +1,20 @@
+//
+//  ChatRoomHeader.swift
+//  MudahChat
+//
+//  Created by Gus Adi on 08/09/22.
+//
+
+import SwiftUI
+
+struct ChatRoomHeader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChatRoomHeader_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatRoomHeader()
+    }
+}
